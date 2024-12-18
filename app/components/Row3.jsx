bc748e0dc8955e327e7c2ae43fe87b0ed921476b
wanglei <3083657638@qq.com>
2024-12-18 17:33:24 +0800

    ro3 编辑

--- app/components/Row3.jsx
+++ app/components/Row3.jsx
@@ -1,4 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {defer} from '@shopify/remix-oxygen';
+import {Await,useLoaderData,Link} from '@remix-run/react';
+import {Suspense} from 'react';
+
+/**
+ * 
+ * @param{LoaderFunctionArgs} args
+ */
+export async function loader(args){
+    const deferredData = loadDeferredData(args);
+
+    const criticalData = await loadCriticalData(args);
+
+    return defer({...deferredData, ...criticalData});
+}
+
+/**
+ * @param {loadDeferredData}
+ */
+async function loadCriticalData({context}){
+    const [{collections}] = await Promise.all([
+        context.storefront.query(FEATURED_COLLECTION_QUERY),
+    ])
+
+    return {
+        featuredCollection:collections.nodes[0],
+    }
+}
 
 const NewSection3 = ()=>{
     return(
@@ -15,5 +43,5 @@ const FEATURED_COLLECTION_QUERY= `#graphal
     fragment NewSection3 on Product {
        id
        title
-        
+
     }`;
